de2f1bd0077f31acd9f2dbc0543b44c35fa16f71
Blainaa <blainaniangi2021@gmail.com>
2026-06-16 19:07:38 +0200

    feat: add comments to main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from cleaning import load_and_clean
 
 def main():
+    # Initialisation de la session Spark connectée au cluster
     spark = SparkSession.builder \
         .appName("Spark Retail Analytics") \
         .master("spark://spark-master:7077") \
@@ -13,6 +14,7 @@ def main():
     print("  SPARK RETAIL - CHARGEMENT DES DONNEES")
     print("="*50)
 
+  # Chargement et nettoyage du dataset
     df = load_and_clean(spark, "/data/online_retail.csv")
 
     print("\nApercu des donnees nettoyees :")
